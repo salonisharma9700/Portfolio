c305169418914d0482885dc784632b2eb3869d3a
Saloni Sharma <salonisharma9700@gmail.com>
2023-10-12 12:42:42 +0530

    First commit

---
+++ s.js
@@ -0,0 +1,18 @@
+ScrollReveal({ 
+    reset: true ,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .about-content,projects-content,contact', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+
+/*typed js*/
+const typed = new Typed('.multiple-text',{
+    strings:['Full Stack Developer','Software Developer','Data Scientist'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
